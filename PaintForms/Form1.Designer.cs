87b4097d8467705203b79cfb7c55521d8e431380
User <User@DESKTOP-RBMPGAB>
2023-09-23 13:09:04 +0700

    Добавьте файлы проекта.

---
+++ PaintForms/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace PaintForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Holst = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ColorsPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Yellow = new System.Windows.Forms.Button();
+            this.Pink = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.Villet = new System.Windows.Forms.Button();
+            this.Blue = new System.Windows.Forms.Button();
+            this.White = new System.Windows.Forms.Button();
+            this.Black = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Width = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.Holst)).BeginInit();
+            this.ColorsPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Width)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Holst
+            // 
+            this.Holst.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Holst.Location = new System.Drawing.Point(2, 134);
+            this.Holst.Name = "Holst";
+            this.Holst.Size = new System.Drawing.Size(1010, 449);
+            this.Holst.TabIndex = 0;
+            this.Holst.TabStop = false;
+            this.Holst.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Holst_MouseDown);
+            this.Holst.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Holst_MouseMove);
+            this.Holst.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Holst_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(876, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear Holst";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(724, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save Holst Bro";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // ColorsPanel
+            // 
+            this.ColorsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorsPanel.Controls.Add(this.label1);
+            this.ColorsPanel.Controls.Add(this.Yellow);
+            this.ColorsPanel.Controls.Add(this.Pink);
+            this.ColorsPanel.Controls.Add(this.Green);
+            this.ColorsPanel.Controls.Add(this.Red);
+            this.ColorsPanel.Controls.Add(this.Villet);
+            this.ColorsPanel.Controls.Add(this.Blue);
+            this.ColorsPanel.Controls.Add(this.White);
+            this.ColorsPanel.Controls.Add(this.Black);
+            this.ColorsPanel.Location = new System.Drawing.Point(181, 16);
+            this.ColorsPanel.Name = "ColorsPanel";
+            this.ColorsPanel.Size = new System.Drawing.Size(150, 78);
+            this.ColorsPanel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Цветошки";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Yellow
+            // 
+            this.Yellow.BackColor = System.Drawing.Color.Yellow;
+            this.Yellow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Yellow.Location = new System.Drawing.Point(41, 32);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(30, 23);
+            this.Yellow.TabIndex = 7;
+            this.Yellow.UseVisualStyleBackColor = false;
+            this.Yellow.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Pink
+            // 
+            this.Pink.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Pink.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pink.Location = new System.Drawing.Point(114, 3);
+            this.Pink.Name = "Pink";
+            this.Pink.Size = new System.Drawing.Size(31, 23);
+            this.Pink.TabIndex = 6;
+            this.Pink.UseVisualStyleBackColor = false;
+            this.Pink.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Green
+            // 
+            this.Green.BackColor = System.Drawing.Color.LimeGreen;
+            this.Green.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Green.Location = new System.Drawing.Point(77, 3);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(31, 23);
+            this.Green.TabIndex = 5;
+            this.Green.UseVisualStyleBackColor = false;
+            this.Green.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Red
+            // 
+            this.Red.BackColor = System.Drawing.Color.Red;
+            this.Red.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Red.Location = new System.Drawing.Point(40, 3);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(31, 23);
+            this.Red.TabIndex = 4;
+            this.Red.UseVisualStyleBackColor = false;
+            this.Red.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Villet
+            // 
+            this.Villet.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.Villet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Villet.Location = new System.Drawing.Point(114, 32);
+            this.Villet.Name = "Villet";
+            this.Villet.Size = new System.Drawing.Size(30, 23);
+            this.Villet.TabIndex = 3;
+            this.Villet.UseVisualStyleBackColor = false;
+            this.Villet.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Blue
+            // 
+            this.Blue.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Blue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Blue.Location = new System.Drawing.Point(78, 32);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(30, 23);
+            this.Blue.TabIndex = 2;
+            this.Blue.UseVisualStyleBackColor = false;
+            this.Blue.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // White
+            // 
+            this.White.BackColor = System.Drawing.Color.White;
+            this.White.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.White.Location = new System.Drawing.Point(4, 32);
+            this.White.Name = "White";
+            this.White.Size = new System.Drawing.Size(30, 23);
+            this.White.TabIndex = 1;
+            this.White.UseVisualStyleBackColor = false;
+            this.White.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Black
+            // 
+            this.Black.BackColor = System.Drawing.Color.Black;
+            this.Black.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Black.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Black.Location = new System.Drawing.Point(4, 3);
+            this.Black.Name = "Black";
+            this.Black.Size = new System.Drawing.Size(30, 23);
+            this.Black.TabIndex = 0;
+            this.Black.UseVisualStyleBackColor = false;
+            this.Black.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Width);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(2, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 78);
+            this.panel1.TabIndex = 4;
+            // 
+            // Width
+            // 
+            this.Width.Location = new System.Drawing.Point(18, 3);
+            this.Width.Name = "Width";
+            this.Width.Size = new System.Drawing.Size(118, 45);
+            this.Width.TabIndex = 1;
+            this.Width.ValueChanged += new System.EventHandler(this.Width_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Толщина";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 595);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ColorsPanel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Holst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Holst)).EndInit();
+            this.ColorsPanel.ResumeLayout(false);
+            this.ColorsPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Width)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Holst;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel ColorsPanel;
+        private System.Windows.Forms.Button Yellow;
+        private System.Windows.Forms.Button Pink;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Button Villet;
+        private System.Windows.Forms.Button Blue;
+        private System.Windows.Forms.Button White;
+        private System.Windows.Forms.Button Black;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar Width;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ PaintForms/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetSize();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private class ArrayPoints
+        {
+            private int index = 0;
+            private Point[] points;
+
+            public ArrayPoints(int size)
+            {
+                if (size <= 0) { size = 2; }
+                points = new Point[size];
+            }
+            public void SetPoint(int x, int y)
+            {
+                if (index >= points.Length)
+                {
+                    index = 0;
+                }
+                points[index] = new Point(x, y);
+                index++;
+            }
+            public void ResetPoint()
+            {
+                index = 0;
+            }
+            public int GetCountPoints()
+            {
+                return index;
+            }
+            public Point[] GetPoints(){
+                return points;
+            }
+        }
+        private bool DrawMouse=false;
+        private ArrayPoints arrayPoints=new ArrayPoints(2);
+        Bitmap map=new Bitmap(100,100);
+        Graphics graphics;
+        Pen pen = new Pen(Color.Black, 3);
+        private void SetSize()
+        {
+            Rectangle rectangle=Screen.PrimaryScreen.Bounds;
+            map=new Bitmap(rectangle.Width, rectangle.Height);
+            graphics = Graphics.FromImage(map);
+            pen.StartCap=System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+        private void Holst_MouseDown(object sender, MouseEventArgs e)
+            {
+             DrawMouse= true;
+            }
+         private void Holst_MouseUp(object sender, MouseEventArgs e)
+            {
+            DrawMouse = false;
+            arrayPoints.ResetPoint();
+        }
+        private void Holst_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!DrawMouse) { return; }
+            arrayPoints.SetPoint(e.X, e.Y);
+            if (arrayPoints.GetCountPoints() >= 2)
+            {
+                graphics.DrawLines(pen,arrayPoints.GetPoints());
+                Holst.Image = map;
+                arrayPoints.SetPoint(e.X,e.Y);
+            }
+        }
+    
+        private void Color_Click(object sender, EventArgs e)
+        {
+            pen.Color=((Button)sender).BackColor;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Holst.BackColor);
+            Holst.Image = map;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "JPG(*.JPG)|*.jpg";
+            if (saveFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                if (Holst.Image == null)
+                {
+                    Holst.Image.Save(saveFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void Width_ValueChanged(object sender, EventArgs e)
+        {
+            pen.Width=Width.Value;
+        }
+    }
+}
